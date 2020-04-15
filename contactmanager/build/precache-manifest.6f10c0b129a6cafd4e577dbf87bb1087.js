b4cdb4e6672ee2aa19192a689c4ffe7b8e822652
robbiemcmullen1981 <43388530+robbiemcmullen1981@users.noreply.github.com>
2020-04-15 22:09:29 +0100

    Initial upload commit

---
+++ contactmanager/build/precache-manifest.6f10c0b129a6cafd4e577dbf87bb1087.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbce96a613ec4051a8da8ec43dde1330",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4ec838846771b7d4340",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2a480d1865383941d470",
+    "url": "/static/js/2.f49f49fc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.f49f49fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4ec838846771b7d4340",
+    "url": "/static/js/main.5e8290f0.chunk.js"
+  },
+  {
+    "revision": "91b3683c09fe04960098",
+    "url": "/static/js/runtime-main.45bf7fa5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
